b8195bdb03a158fd76a10a91da5405aee2730d9b
Muhammed Humam Hossain <66829442+humam-hossain@users.noreply.github.com>
2023-06-23 00:08:37 +0600

    first complete implementation of the algorithm

--- simulate_life/main.cpp
+++ simulate_life/main.cpp
@@ -1,15 +1,26 @@
-#include<stdio.h>
+#include<iostream>
 #include<stdlib.h>
 #include<time.h>
+#include<math.h>
+#include<string>
 
-#define HEIGHT 10
-#define WIDTH 10
+#define HEIGHT 100
+#define WIDTH 100
 
 float grid[HEIGHT][WIDTH] = { 0.0f };
+float grid_diff[HEIGHT][WIDTH] = { 0.0f };
+
 char level[] = " .-=coaA@#";
 #define level_count (sizeof(level) / sizeof(level[0]) - 1)
 
-float ra = 21;
+float ra = 11.0f;
+float alpha = 0.028f;
+// float alpha = 0.147f;
+float b1 = 0.278f;
+float b2 = 0.365f;
+float d1 = 0.267f;
+float d2 = 0.445f;
+float dt = 0.1f;
 
 float rand_float(void)
 {
@@ -17,21 +28,27 @@ float rand_float(void)
 }
 
 void random_grid(void) {
-    for (size_t y = 0; y < HEIGHT; ++y) {
-        for (size_t x = 0; x < WIDTH; ++x) {
+    size_t w = WIDTH / 3;
+    size_t h = HEIGHT / 3;
+    for (size_t dy = 0; dy < HEIGHT/2; ++dy) {
+        for (size_t dx = 0; dx < WIDTH/2; ++dx) {
+            size_t y = dy + HEIGHT / 2 - h / 2;
+            size_t x = dx + WIDTH / 2 - w / 2;
+
             grid[y][x] = rand_float();
         }
     }
 }
 
-void display_grid(void) {
+void display_grid(float grid[HEIGHT][WIDTH]) {
     for (size_t y = 0; y < HEIGHT; ++y) {
         for (size_t x = 0; x < WIDTH; ++x) {
             char c = level[(int)(grid[y][x] * (level_count - 1))];
 
-            printf("%c", c);
+            fputc(c, stdout);
+            fputc(c, stdout);
         }
-        printf("\n");
+        fputc('\n', stdout);
     }
 }
 
@@ -39,42 +56,78 @@ int emod(int a, int b) {
     return (a % b + b) % b;
 }
 
-int main()
-{
-    srand(time(0));
-    random_grid();
+float sigma1(float x, float a) {
+    return 1.0f / (1.0f + expf(-(x - a) * 4 / alpha));
+}
 
-    int cx = 0;
-    int cy = 0;
-    float m = 0;
-    float n = 0;
-    int M = 0;
-    int N = 0;
+float sigma2(float x, float a, float b) {
+    return sigma1(x, a) * (1 - sigma1(x, b));
+}
 
-    int ri = ra / 3;
+float sigmam(float x, float y, float m) {
+    return x * (1 - sigma1(m, 0.5f)) + y * sigma1(m, 0.5f);
+}
 
-    for (int dy = -(ra - 1); dy < ra; ++dy) {
-        for (int dx = -(ra - 1); dx < ra; ++dx) {
-            int y = emod(cy + dy, HEIGHT);
-            int x = emod(cx + dx, WIDTH);
+float s(float n, float m) {
+    return sigma2(n, sigmam(b1, d1, m), sigmam(b2, d2, m));
+}
 
-            if (dx * dx + dy * dy <= ri * ri) {
-                m += grid[y][x];
-                M += 1;
+void compute_grid_diff(void) {
+    for (int cy = 0; cy < HEIGHT; ++cy) {
+        for (int cx = 0; cx < WIDTH; ++cx) {
+            float m = 0;
+            float n = 0;
+            float M = 0;
+            float N = 0;
+
+            float ri = ra / 3;
+
+            for (int dy = -(ra - 1); dy < ra; ++dy) {
+                for (int dx = -(ra - 1); dx < ra; ++dx) {
+                    int y = emod(cy + dy, HEIGHT);
+                    int x = emod(cx + dx, WIDTH);
+
+                    if (dx * dx + dy * dy <= ri * ri) {
+                        m += grid[y][x];
+                        M += 1;
+                    }
+                    else if (dx * dy + dy * dy <= ra * ra) {
+                        n += grid[y][x];
+                        N += 1;
+                    }
+                }
             }
-            else if (dx * dy + dy * dy <= ra * ra) {
-                n += grid[y][x];
-                N += 1;
-            }
+
+            m /= M;
+            n /= N;
+
+            float q = s(n, m);
+            grid_diff[cy][cx] = 2*q - 1;
         }
     }
+}
 
-    m /= M;
-    n /= N;
+void clamp(float* x, float l, float h) {
+    if (*x < l) *x = l;
+    if (*x > h) *x = h;
+}
 
-    printf("m = %f \t n=%f\n", m, n);
+int main()
+{
+    srand((unsigned int) time(0));
+    random_grid();
 
-    // display_grid();
+    for (;;) {
+        display_grid(grid);
+        for (size_t y = 0; y < HEIGHT; ++y) {
+            for (size_t x = 0; x < WIDTH; ++x) {
+                grid[y][x] += dt * grid_diff[y][x];
+                clamp(&grid[y][x], 0, 1);
+            }
+        }
+        compute_grid_diff();
+        system("cls");
+    }
 
     return 0;
 }
