5d7be0726fb698b79849a861d17fa7641205782f
Muhammed Humam Hossain <66829442+humam-hossain@users.noreply.github.com>
2023-07-10 22:54:24 +0600

    Update smoothlife.fs

--- smoothlife.fs
+++ smoothlife.fs
@@ -1,17 +1,14 @@
 #version 330
 
-// Input vertex attributes (from vertex shader)
 in vec2 fragTexCoord;
 in vec4 fragColor;
 
-// Input uniform values
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
 
-// Output fragment color
 out vec4 finalColor;
 
 void main()
 {
-    finalColor = texture(texture0, fragTexCoord) * vec4(0,1,0,1);
+	finalColor = texture(texture0, fragTexCoord)*vec4(0,0.97,0,1);
 }
