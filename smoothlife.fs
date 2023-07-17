26a7920c637b48170c26ee017df23edede0c1248
Muhammed Humam Hossain <66829442+humam-hossain@users.noreply.github.com>
2023-07-17 23:14:06 +0600

    basics working in shader

--- smoothlife.fs
+++ smoothlife.fs
@@ -8,7 +8,94 @@ uniform vec4 colDiffuse;
 
 out vec4 finalColor;
 
-void main()
+uniform vec2 resolution;
+
+// global values
+float ra = 11;
+float alpha = 0.028;
+// float alpha = 0.147;
+float b1 = 0.278;
+float b2 = 0.365;
+float d1 = 0.267;
+float d2 = 0.445;
+float dt = 0.01;
+
+#define PI 3.14159265359
+
+// functions
+// int emod(int a, int b)
+// {
+//     return (a % b + b) % b;
+// }
+
+float sigma1(float x, float a)
 {
-	finalColor = texture(texture0, fragTexCoord)*vec4(0,0.97,0,1);
+    return 1.0 / (1.0 + exp(-(x - a) * 4 / alpha));
+}
+
+float sigma2(float x, float a, float b)
+{
+    return sigma1(x, a) * (1 - sigma1(x, b));
+}
+
+float sigmam(float x, float y, float m)
+{
+    return x * (1 - sigma1(m, 0.5)) + y * sigma1(m, 0.5);
+}
+
+float s(float n, float m)
+{
+    return sigma2(n, sigmam(b1, d1, m), sigmam(b2, d2, m));
+}
+
+float grid(float x, float y){
+	float tx = float(x)/resolution.x;
+	float ty = float(y)/resolution.y;
+	vec4 t = texture(texture0, vec2(tx, ty));
+
+	return max(max(t.x, t.y), t.z);
+}
+
+void main() {
+#if 1
+	float cx = fragTexCoord.x * resolution.x;
+	float cy = (1 - fragTexCoord.y) * resolution.y;
+
+	float ri = ra / 3;
+	float m = 0; 
+	float M = PI * ri * ri;
+	float n = 0; 
+	float N = PI * ra * ra;
+
+
+	for (float dy = -(ra - 1); dy < ra; dy += 1.0){
+		for (float dx = -(ra - 1); dx < ra; dx += 1.0){
+			// int y = emod(cy + dy, int(resolution.x));
+			// int x = emod(cx + dx, int(resolution.y));
+
+			float x = cx + dx;
+			float y = cy + dy;
+
+			if (dx * dx + dy * dy <= ri * ri)
+			{
+				m += grid(x, y);
+			}
+			else if (dx * dy + dy * dy <= ra * ra)
+			{
+				n += grid(x, y);
+			}
+		}
+	}
+
+	m /= M;
+	n /= N;
+
+	float q = s(n, m);
+	float diff = 2 * q - 1;
+	float v = grid(cx, cy) + dt*diff;
+#endif
+
+	// finalColor = texture(texture0, fragTexCoord)*vec4(0,0.95,0,1);
+	// finalColor = texture(texture0, vec2(fragTexCoord.x, -fragTexCoord.y));
+	finalColor = vec4(v, v, v, 1);
 }
--- smoothlife_gpu.cpp
+++ smoothlife_gpu.cpp
@@ -8,14 +8,22 @@ int main() {
 	SetTargetFPS(60);
 
 	// Image image = GenImagePerlinNoise(screen_width, screen_height, 0, 0, 5.0f);
+	// Image image = GenImageWhiteNoise(screen_width, screen_height, 0.2f);
 	Image image = GenImageCellular(screen_width, screen_height, screen_height/6);
 	
 	RenderTexture2D state[2];
+	
 	state[0] = LoadRenderTexture(screen_width, screen_height);
-	state[1] = LoadRenderTexture(screen_width, screen_height);
+	SetTextureWrap(state[0].texture, TEXTURE_WRAP_REPEAT);
 	UpdateTexture(state[0].texture, image.data);
 
+	state[1] = LoadRenderTexture(screen_width, screen_height);
+	SetTextureWrap(state[1].texture, TEXTURE_WRAP_REPEAT);
+
 	Shader shader = LoadShader(NULL, "./smoothlife.fs");
+	Vector2 resolution = {screen_width, screen_height};
+	int resolution_loc = GetShaderLocation(shader, "resolution");
+	SetShaderValue(shader, resolution_loc, &resolution, SHADER_UNIFORM_VEC2);
 
 	size_t i = 0;
 
@@ -23,13 +31,13 @@ int main() {
 		BeginTextureMode(state[1 - i]);
 			ClearBackground(BLACK);
 			BeginShaderMode(shader);
-				DrawTexture(state[i].texture, 0, 0, BLUE);
+				DrawTexture(state[i].texture, 0, 0, WHITE);
 			EndShaderMode();
 		EndTextureMode();
 		
 		BeginDrawing();
 			ClearBackground(BLACK);
-			DrawTexture(state[1 - i].texture, 0, 0, BLUE);
+			DrawTexture(state[1 - i].texture, 0, 0, WHITE);
 		EndDrawing();
 
 		i = 1 - i;
